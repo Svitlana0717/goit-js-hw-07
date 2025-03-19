fa76aa25546d3d245a628d9424502fa566807229
Svitlana0717 <svaitlanasorokina71@gmail.com>
2025-03-19 20:01:45 +0100

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,11 @@
-// Вибираємо елемент ul#categories
-const categories = document.querySelector('#categories');
+const categoriesList = document.querySelector("#categories");
+const categoryItems = categoriesList.querySelectorAll(".item");
 
-// Отримуємо всі елементи li.item у списку ul#categories
-const categoryItems = categories.querySelectorAll('.item');
+console.log(`Number of categories: ${categoryItems.length}`);
 
-// Виводимо кількість категорій
-console.log(`Кількість категорій: ${categoryItems.length}`);
-
-// Для кожного елемента li.item знаходимо текст заголовка (тег <h2>) і кількість вкладених елементів <li>
-categoryItems.forEach(item => {
-  const title = item.querySelector('h2').textContent;
-  const itemsCount = item.querySelectorAll('li').length;
-  console.log(`Категорія: ${title}\nКількість елементів: ${itemsCount}`);
+categoryItems.forEach((item) => {
+  const title = item.querySelector("h2").textContent;
+  const elementsCount = item.querySelectorAll("ul li").length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
 });
