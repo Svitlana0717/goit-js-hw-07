a49965956585489ab3b75622124a44e5e207f59e
Svitlana0717 <svaitlanasorokina71@gmail.com>
2025-03-20 19:30:45 +0100

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,24 +1,17 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
 
-// Функція для генерації випадкового кольору у hex-форматі
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-}
+  const button = document.querySelector(".change-color");
+  const colorSpan = document.querySelector(".color");
+  const body = document.body;
 
-// Вибираємо елементи button.change-color та span.color
-const changeColorButton = document.querySelector('.change-color');
-const colorSpan = document.querySelector('.color');
-
-// Додаємо слухача подій для button.change-color
-changeColorButton.addEventListener('click', () => {
-  // Генеруємо випадковий колір
-  const newColor = getRandomHexColor();
-
-  // Змінюємо колір фону body та вміст span.color
-  document.body.style.backgroundColor = newColor;
-  colorSpan.textContent = newColor;
+  button.addEventListener("click", () => {
+    const newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor;
+    colorSpan.textContent = newColor;
+  });
 });
